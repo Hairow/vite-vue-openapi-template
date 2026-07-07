ff99de3d5266a5927ae1d4142ae34b8a619c51a9
hairow <hairow@foxmail.com>
2026-07-07 12:19:00 +0800

    t

--- src/views/TodosSimpleView.vue
+++ src/views/TodosSimpleView.vue
@@ -86,6 +86,7 @@ const newTodo = ref({ title: '', description: '' });
 
 const { data: todos, isLoading } = useQuery(getTodosOptions());
 
+
 const { mutate: createTodo, isPending: isCreating } = useMutation({
   ...createTodoMutation(),
   onSuccess: () => {
